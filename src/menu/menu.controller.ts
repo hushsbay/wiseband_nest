96a03c15ad4467dc007713bdf704aaf192211317
hushsbay <hushsbay@gmail.com>
2025-01-23 15:33:41 +0900

    .

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -11,20 +11,8 @@ export class MenuController {
     @Post('qry')
     qry(@Body() dto: Record<string, any>) { return this.menuSvc.qry(dto) }
 
+    @HttpCode(HttpStatus.OK)
+    @Post('qry')
+    qryChan(@Body() dto: Record<string, any>) { return this.menuSvc.qry(dto) }
+
 }
-
-
-// import { Controller, HttpCode, HttpStatus, Post, Body } from '@nestjs/common'
-
-// import { MenuService } from 'src/menu/menu.service'
-
-// @Controller('menu')
-// export class MenuController {
-
-//     constructor(private readonly menuSvc: MenuService) {}
-
-//     @HttpCode(HttpStatus.OK)
-//     @Post('qry')
-//     qry(@Body() dto: Record<string, any>) { return this.menuSvc.qry(dto) }
-
-// }
