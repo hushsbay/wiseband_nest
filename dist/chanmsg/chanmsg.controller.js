1e575f99f0c81096cc60050d6dc3497a9da3d1d3
hushsbay <hushsbay@gmail.com>
2025-08-28 04:51:03 +0900

    .

--- dist/chanmsg/chanmsg.controller.js
+++ dist/chanmsg/chanmsg.controller.js
@@ -25,11 +25,7 @@ let ChanmsgController = class ChanmsgController {
         this.chanmsgSvc = chanmsgSvc;
         this.configService = configService;
     }
-    qry(dto) {
-        const xx = this.chanmsgSvc.qry(dto);
-        console.log("@@@@@@@@@@@@");
-        return xx;
-    }
+    qry(dto) { return this.chanmsgSvc.qry(dto); }
     qryChanMstDtl(dto) { return this.chanmsgSvc.qryChanMstDtl(dto); }
     qryOneMsgNotYet(dto) { return this.chanmsgSvc.qryOneMsgNotYet(dto); }
     qryMsg(dto) { return this.chanmsgSvc.qryMsg(dto); }
