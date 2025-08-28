3dc85ffd50162e810bc600951fbe0454b1209426
hushsbay <hushsbay@gmail.com>
2025-08-28 10:24:34 +0900

    .

--- src/socket/events.gateway.ts
+++ src/socket/events.gateway.ts
@@ -7,7 +7,7 @@ import { MessageBody, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, W
 import { Server, Socket } from 'socket.io'
 import * as hush from 'src/common/common'
 
-@WebSocketGateway(parseInt(appConfig().sock.port), { 
+@WebSocketGateway(3052, { 
     transports: ['websocket', 'polling'], cors: { origin: hush.cons.corsOrigin }, namespace: hush.cons.appName, pingTimeout: 5000, pingInterval: 25000 
 })
 export class EventsGateway implements OnGatewayDisconnect { //OnGatewayConnection
