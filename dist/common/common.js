ba9005af22cc0d1f87871e1f7f02134668045fcb
hushsbay <hushsbay@gmail.com>
2025-08-28 10:01:52 +0900

    .

--- dist/common/common.js
+++ dist/common/common.js
@@ -59,7 +59,7 @@ var Msg;
 })(Msg || (exports.Msg = Msg = {}));
 exports.cons = {
     appName: 'WiSEBand',
-    corsOrigin: ['https://hushsbay.com:446', 'http://localhost:5173'],
+    corsOrigin: ['http://localhost:5173', 'https://hushsbay.com:446', 'https://hushsbay.com:3052'],
     otpDiffMax: 1,
     rowsCnt: 30,
     rowsCntForNotyet: 1000,
