319cde161cff51cfcbc7d3decf78b7f05b614237
hushsbay <hushsbay@gmail.com>
2025-09-21 12:13:34 +0900

    .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,7 +8,7 @@ export class UserController {
     constructor(private readonly userSvc: UserService) {}
 
     @Get('getUserInfo') //이미지 Caching이 필요한 Get 방식으로 처리
-    @Header('Cache-Control', 'no-cache') //= @Header('Cache-Control', 'max-age=0, must-revalidate')
+    @Header('Cache-Control', 'max-age=0, must-revalidate') //= @Header('Cache-Control', 'no-cache')
     //현재 max-age 동작 안되고 있으나 이미지 캐싱에만 사용할 것이므로 no-cache나 max-age=0, must-revalidate로 설정하기로 함 (서버 기본 설정값이기도 함)
     //참고) no-cache는 캐시는 가능한데 사용할 때마다 서버에 재검증하는 것이고 진짜 캐싱하지 않는 것은 no-store임
     //실제로 no-cache로 설정후 이미지를 변경하면 200/201로 내려오고 그 다음에 요청시 바로 304 not modified로 잘 되는 것을 확인함 (브라우저가 알아서 처리하고 있음)
